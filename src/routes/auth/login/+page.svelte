c3ebec896984d0f9ce86d3819cdc400dfdd4625b
ostSTRUPpen <62558669+ostSTRUPpen@users.noreply.github.com>
2023-06-27 23:34:36 +0200

    working on auth

---
+++ src/routes/auth/login/+page.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+
+const { data, error } = await supabase.auth.signUp({
+      email: 'example@email.com',
+        password: 'example-password',
+})
+})
+</script>
+
+<div>
+<form>
+<label for="emai">E-mail:</label>
+<input name="email" type="email"/> <br/>
+<label for="password">Heslo:</label>
+<input nam="password" type="password"/>
+</form>
+</div>
+
