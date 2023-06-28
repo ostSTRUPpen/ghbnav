42c0d8738eacd40084ead951d92e525967993be1
VV <vojvol@post.cz>
2023-06-28 07:15:17 +0200

    Merge conflict - probably

--- src/lib/supabaseClient.ts
+++ src/lib/supabaseClient.ts
@@ -1,4 +1,4 @@
 import { createClient } from '@supabase/supabase-js';
-import { SUPABASE_URL, SUPABASE_API_KEY } from '$env/static/private';
+import { PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_ANON_KEY } from '$env/static/public';
 
-export const supabase = createClient(SUPABASE_URL, SUPABASE_API_KEY);
+export const supabase = createClient(PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_ANON_KEY);
---
+++ src/routes/auth/login/+page.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	import { Auth } from '@supabase/auth-ui-svelte';
+	import { ThemeSupa } from '@supabase/auth-ui-shared';
+
+	export let data;
+	//appearance={{ theme: ThemeSupa, style: { input: 'color: #fff' } }}
+</script>
+
+<svelte:head>
+	<title>User Management</title>
+</svelte:head>
+
+<Auth
+	supabaseClient={data.supabase}
+	view="magic_link"
+	redirectTo={`${data.url}/auth/callback`}
+	showLinks={false}
+/>
