dc87dd1da3d6a087d32b22ea4ad8820a871bfc76
VV <vojvol@post.cz>
2023-06-28 19:05:39 +0200

    Added from, to selectors

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,5 +1,10 @@
-import { supabase } from '$lib/supabaseClient'
+import { supabase } from '$lib/supabaseClient';
 
 export async function load() {
-    const { data } = await supabase.from('markers').select("id, display_nam")
+	const { data } = await supabase.from('markers').select('id, display_name');
+
+	//console.log(data);
+	return {
+		endingPoints: data ?? []
+	};
 }
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,25 +1,47 @@
 <script lang="ts">
- export let data
- 
+	import { goto } from '$app/navigation';
+
+	export let data;
+	let { endingPoints } = data;
+	$: ({ endingPoints } = data);
+
+	let navFrom: string;
+	let navTo: string;
+
+	function navFromTo() {
+		console.log(navFrom + ' -> ' + navTo);
+		goto(`/map/${navFrom}/${navTo}`, { replaceState: false });
+	}
 </script>
 
-
 <div>
-<div>
-<label for="from">Odkud:</label>
-<select name="from">
-<select>
+	<div>
+		<!--<label for="from">Odkud:</label>
+		<input list="ending_points" id="from" name="from" bind:value={navFrom} />
+		<br />
+		<label for="to">Kam:</label>
+		<input list="ending_points" id="to" name="to" bind:value={navTo} />
+		<datalist id="ending_points">
+			{#each endingPoints as endPoint}
+				<option value={endPoint.display_name}/>
+			{/each}
+		</datalist>-->
+		<label for="from">Odkud:</label>
+		<select id="from" name="from" bind:value={navFrom}>
+			{#each endingPoints as endPoint}
+				<option value={endPoint.id}>{endPoint.display_name}</option>
+			{/each}
+		</select>
+		<label for="to">Kam:</label>
+		<select id="to" name="to" bind:value={navTo}>
+			{#each endingPoints as endPoint}
+				<option value={endPoint.id}>{endPoint.display_name}</option>
+			{/each}
+		</select>
+		<button on:click={navFromTo}>Navigovat</button>
+	</div>
 
-<label for="to">Kam:</label>
-<select name="to">
-</select>
-</div>
+	<div>Skenovat qr kod</div>
 
-<div>
-Skenovat qr kod
-</div>
-
-<div>
-caste cesty
-</div>
+	<div>caste cesty</div>
 </div>
