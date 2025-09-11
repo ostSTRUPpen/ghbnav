653194949a58e4f8f9195a29570694ff8b36cacb
ostSTRUPpen <vojvol@post.cz>
2025-09-11 22:06:45 +0200

    Added analytics

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -1,3 +1,6 @@
+import { dev } from '$app/environment';
+import { injectAnalytics } from '@vercel/analytics/sveltekit';
 import { injectSpeedInsights } from '@vercel/speed-insights/sveltekit';
 
 injectSpeedInsights();
+injectAnalytics({ mode: dev ? 'development' : 'production' });
